ff6552f91681a693afb31d75302a6c7d0d2c0d6f
Oloufemi Adekambi <romaric.adekambi.ext@eulerhermes.com>
2021-03-11 09:02:05 +0100

    Finalize the calculator

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,15 +1,68 @@
 
-
-function chooseRightMode() {
+function manageUserResponse(text='') {
     let modeChoiceText = 'What operation do you want to accomplish ? \n\n 1 - Addition\n 2 - Soustraction\n 3 - Multiplication\n' 
                         + ' 4 - Division';
-    let mode;
-    do {
-        mode = Number(prompt(modeChoiceText));
-        
-    } while (isNaN(mode) || mode < 1 || mode > 4);
-    return mode;
+    let result;
+    
+    if (text) {
+    result = Number(prompt(text));
+    while(isNaN(result)) {
+        alert('Veuillez s\'il vous plaît saisir un nombre ');
+        result = Number(prompt(text));
+        }
+    } else {
+        do {
+            result = Number(prompt(modeChoiceText));
+            
+        } while (isNaN(result) || result < 1 || result > 4);
+    }
+    
+    return result;
 }
 
-const choice = chooseRightMode();
-alert(' Mode ' + choice);
+function calculator(mode, firstNumber, secondNumber) {
+    let result;
+    try {
+        switch (mode) {
+            case 1:
+                result = firstNumber + secondNumber;
+                break;
+            case 2:
+                result = firstNumber - secondNumber;                
+                break;
+            case 3:
+                result = firstNumber * secondNumber;                
+                break;
+            case 4:
+                if(secondNumber === 0) {
+                    throw new Error('Vous ne pouvez pas diviser par 0');
+                } 
+                result = (firstNumber / secondNumber).toFixed(2);                
+                break;
+            default:
+                throw new Error('Une erreur est survenue pendant l\'opération');
+        }
+    }
+    catch(e) {
+        result = e.message;
+    }
+    return result;
+}
+
+// Entering in this function you are in a calculator mode which let you do multiple operations
+function calculatorMode() {
+const choice = manageUserResponse();
+const firstNumber = manageUserResponse('Veuillez s\'il vous plaît saisir le premier nombre');
+const secondNumber = manageUserResponse('Veuillez s\'il vous plaît saisir le deuxième nombre');
+let result = calculator(choice, firstNumber, secondNumber);
+if(!isNaN(result)) {
+    alert('Le résultat de votre opération: ' + result);
+}else {
+    alert(result);
+}
+}
+
+let numberOfOperations = manageUserResponse('Combien d\'opérations souhaitez-vous effectuer ?');
+for (let index = 0; index < numberOfOperations; index++) {
+    calculatorMode();
+}
