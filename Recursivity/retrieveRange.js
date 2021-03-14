29bcdd5b850e07b3844a4fe187880c56a2805f6c
Oloufemi Adekambi <romaric.adekambi.ext@eulerhermes.com>
2021-03-14 12:05:09 +0100

    retrieve two numbers range  operation using recursion

---
+++ Recursivity/retrieveRange.js
@@ -0,0 +1,8 @@
+let result = new Array(0);
+
+function range(firstValue, secondValue) {
+    if (firstValue < secondValue) {
+        alert('La deuxième valeur doit être supérieure à la première');
+    }
+    
+}
