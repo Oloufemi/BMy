66e916a49941ffe8ffa3390adfa35df7957c2d73
Oloufemi Adekambi <romaric.adekambi.ext@eulerhermes.com>
2021-03-18 08:12:01 +0100

    Loop for .. of, show associatif table

--- challenges/challenges.js
+++ challenges/challenges.js
@@ -1,5 +1,22 @@
-let players = ['Kobe', 'Lebron','Jordan',4];
+// Loop for ... of
+// let players = ['Kobe', 'Lebron','Jordan',4];
 
-for (const player of players) {
-    console.log(player);
+// for (const player of players) {
+//     console.log(player);
+// }
+
+// function to show associatif table 
+function showAssociatifTable(myTable) {
+    let result = '';
+    for (const prop in myTable) {
+        result += prop + ': ' + myTable[prop] + '\n';
+    }
+    console.log(result);
 }
+
+let table = {
+    'firstname': 'Rui',
+    'lastname': 'Hachimura',
+    'position': 'Small forward',
+}
+showAssociatifTable(table);
