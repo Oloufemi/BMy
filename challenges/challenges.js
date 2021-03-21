3a9c4f5687b5508ba86a7e5f60025ac916d2d46a
Oloufemi Adekambi <romaric.adekambi.ext@eulerhermes.com>
2021-03-21 18:05:51 +0100

    create an object

--- challenges/challenges.js
+++ challenges/challenges.js
@@ -1,22 +1,39 @@
-// Loop for ... of
-// let players = ['Kobe', 'Lebron','Jordan',4];
+/**
+ * Loop for ... of
+ */
 
+// let players = ['Kobe', 'Lebron','Jordan',4];
 // for (const player of players) {
 //     console.log(player);
 // }
 
-// function to show associatif table 
-function showAssociatifTable(myTable) {
-    let result = '';
-    for (const prop in myTable) {
-        result += prop + ': ' + myTable[prop] + '\n';
-    }
-    console.log(result);
-}
+/**
+ *  function to show associatif table 
+ */
 
-let table = {
-    'firstname': 'Rui',
-    'lastname': 'Hachimura',
-    'position': 'Small forward',
+// function showAssociatifTable(myTable) {
+//     let result = '';
+//     for (const prop in myTable) {
+//         result += prop + ': ' + myTable[prop] + '\n';
+//     }
+//     console.log(result);
+// }
+
+// let table = {
+//     'firstname': 'Rui',
+//     'lastname': 'Hachimura',
+//     'position': 'Small forward',
+// }
+// showAssociatifTable(table);
+
+/**
+ * Objects
+ */
+
+let dog = {
+    race : 'german shepherd',
+    hair: 'short',
+    barking : () => {
+        console.log('waf waf');
+    }
 }
-showAssociatifTable(table);
