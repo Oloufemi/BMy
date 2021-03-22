8bf1e7a00b64d1a33b214f760a0c2857a1c54b24
Oloufemi Adekambi <romaric.adekambi.ext@eulerhermes.com>
2021-03-22 08:43:31 +0100

    select element(s) in DOM

---
+++ DOM/dom.js
@@ -0,0 +1,7 @@
+
+/** Selectors */
+// let paragraphs = document.querySelectorAll('p');
+// let headerElement = document.querySelector('header');
+
+let h1 = document.querySelector('h1');
+console.log(h1);
