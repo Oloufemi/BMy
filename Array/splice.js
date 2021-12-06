e35cc815b7ffdb072213fb2541edde617a357aa4
Oloufemi Adekambi <romaric.adekambi.ext@eulerhermes.com>
2021-12-06 16:00:42 +0100

    test ssh again

--- Array/splice.js
+++ Array/splice.js
@@ -1,8 +1,7 @@
-
 let twoDTable = [
-    ['test 1', 'test2'],
-    ['test 11', 'test22']
+  ['test 1', 'test2'],
+  ['test 11', 'test22'],
 ];
 
-twoDTable.splice(2,0,['splice', 'splice2']);
+twoDTable.splice(2, 0, ['splice', 'splice2']);
 console.log(twoDTable);
